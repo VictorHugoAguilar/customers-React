11805edc9dd241681e54de9879d19bb6d40048bf
victor hugo <victor03016@gmail.com>
2019-09-07 20:31:14 +0200

    creacion del componente para editar el customer

---
+++ src/components/CustomerEdit.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CustomerEdit = props => {
+    const { name, age, dni } = props;
+    return (
+        <div>
+            <h2>Edición de cliente</h2>
+            <h3>
+                Nombre: {name} / Dni: {dni} / Edad: {age}
+            </h3>
+        </div>
+    );
+};
+
+CustomerEdit.propTypes = {
+    name: PropTypes.string,
+    dni: PropTypes.string,
+    age: PropTypes.number
+};
+
+export default CustomerEdit;
