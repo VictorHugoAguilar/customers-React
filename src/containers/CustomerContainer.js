72a8ff1ba194ce82ba676e4395ba2dafac77e764
victor hugo <victor03016@gmail.com>
2019-09-28 17:21:59 +0200

    Utilizcion de children en ruteos

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -3,14 +3,28 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import AppFrame from "../components/AppFrame";
 import { getCustomerByDni } from "../selectors/customers";
+import  { Route} from 'react-router-dom';
 
 class CustomerContainer extends Component {
+        //<p>Datos del cliente {this.props.customer.name}</p>
+
+        /* Utilizamos children que nos permite un rute de otro nivel
+            no solmante en el nivel principal si no que dentro de cada 
+            componente
+        */
+
+    renderBody = () => (
+        <Route path="/customers/:dni/edit" children={
+            ({ match }) => ( match ? <p>Es edicion</p> : <p>no es edicion</p> )
+        } />
+    )
+
     render() {
         return (
             <div>
                 <AppFrame
                     header={`Cliente ${this.props.dni}`}
-                    body={<p>Datos del cliente {this.props.customer.name}</p>}
+                    body={this.renderBody()}
                 ></AppFrame>
             </div>
         );
