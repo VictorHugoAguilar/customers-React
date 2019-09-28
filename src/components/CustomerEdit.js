f4463ed00bf0014a42175741d50ae4e4fe3a714d
victor hugo <victor03016@gmail.com>
2019-09-28 18:10:00 +0200

    Creación de formulario

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,14 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { reduxForm, Field} from 'redux-form';
 
 const CustomerEdit = props => {
     const { name, age, dni } = props;
     return (
         <div>
             <h2>Edición de cliente</h2>
-            <h3>
-                Nombre: {name} / Dni: {dni} / Edad: {age}
-            </h3>
+            <form>
+                <div>
+                    <label htmlFor="name">Nombre</label>
+                    <Field name="name" component="input" type="text"></Field>
+                    
+                </div>
+                <div>
+                    <label htmlFor="dni">Dni</label>
+                    <Field name="dni" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Edad</label>
+                    <Field name="age" component="input" typpe="number"></Field>
+                </div>
+            </form>
+
         </div>
     );
 };
@@ -19,4 +33,4 @@ CustomerEdit.propTypes = {
     age: PropTypes.number
 };
 
-export default CustomerEdit;
+export default reduxForm({ form: 'CustomerEdit'})( CustomerEdit);
