89e4218059672bb624feaa943308edf98b007549
victor hugo <victor03016@gmail.com>
2019-09-28 17:29:34 +0200

    Update CustomerContainer.js

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -3,21 +3,46 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import AppFrame from "../components/AppFrame";
 import { getCustomerByDni } from "../selectors/customers";
-import  { Route} from 'react-router-dom';
+import { Route } from "react-router-dom";
+
+// importamos los componentes
+import CustomerEdit from "./../components/CustomerEdit";
+import CustomerData from "./../components/CustomerData";
 
 class CustomerContainer extends Component {
-        //<p>Datos del cliente {this.props.customer.name}</p>
+    //<p>Datos del cliente {this.props.customer.name}</p>
 
-        /* Utilizamos children que nos permite un rute de otro nivel
-            no solmante en el nivel principal si no que dentro de cada 
-            componente
-        */
+    /* 
+        Utilizamos children que nos permite un rute de otro nivel
+        no solmante en el nivel principal si no que dentro de cada 
+        componente
+    */
 
+    /*
+        Podemos utilizar cualquiera de las dos siguiente manera para enviar
+        los datos entre los componentes <CustomerEdit {...this.props.customer} />
+        o  <CustomerData
+                        age={this.props.customer.age}
+                        dni={this.props.customer.dni}
+                        name={this.props.customer.name}
+                    />
+    */
     renderBody = () => (
-        <Route path="/customers/:dni/edit" children={
-            ({ match }) => ( match ? <p>Es edicion</p> : <p>no es edicion</p> )
-        } />
-    )
+        <Route
+            path="/customers/:dni/edit"
+            children={({ match }) =>
+                match ? (
+                    <CustomerEdit {...this.props.customer} />
+                ) : (
+                    <CustomerData
+                        age={this.props.customer.age}
+                        dni={this.props.customer.dni}
+                        name={this.props.customer.name}
+                    />
+                )
+            }
+        />
+    );
 
     render() {
         return (
@@ -33,12 +58,11 @@ class CustomerContainer extends Component {
 
 const mapStateToProps = (state, props) => ({
     customer: getCustomerByDni(state, props)
-    
 });
 
 CustomerContainer.propTypes = { dni: PropTypes.string.isRequired };
 
-export default connect( 
+export default connect(
     mapStateToProps,
     null
 )(CustomerContainer);
