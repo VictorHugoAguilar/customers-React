57300f899a6a4b68d2bdc549dba4c13b4bfc2d21
victor hugo <victor03016@gmail.com>
2019-09-29 18:05:30 +0200

    Añadir varias validaciones personalizadas dentro del validate entre corchetes creando un array

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -3,19 +3,17 @@ import PropTypes from "prop-types";
 import { reduxForm, Field } from "redux-form";
 import { setPropsAsInitial } from "../helpers/setPropsAsInitial";
 
-const isRequired = value => (
-    !value && "Este campo es requerido"
-);
+const isRequired = value => !value && "Este campo es requerido";
 
-const MyField = ({input, meta}) => (
+const MyField = ({ input, meta, type }) => (
     <div>
-        <input {...input} type="text" />
-        {
-            meta.touched && meta.error && <span>{meta.error}</span>
-        }
+        <input {...input} type={!type ? "text" : type} />
+        {meta.touched && meta.error && <span>{meta.error}</span>}
         <span></span>
     </div>
 );
+
+const isNumber = value => isNaN(Number(value)) && "El campo debe ser un número";
 
 const CustomerEdit = props => {
     // const { name, age, dni } = props;
@@ -25,20 +23,30 @@ const CustomerEdit = props => {
             <form>
                 <div>
                     <label htmlFor="name">Nombre</label>
-                    <Field 
-                        name="name" 
-                        component={MyField} 
+                    <Field
+                        name="name"
+                        component={MyField}
                         type="text"
                         validate={isRequired}
                     ></Field>
                 </div>
                 <div>
                     <label htmlFor="dni">Dni</label>
-                    <Field name="dni" component={MyField} validate={isRequired} type="text"></Field>
+                    <Field
+                        name="dni"
+                        component={MyField}
+                        validate={isRequired}
+                        type="text"
+                    ></Field>
                 </div>
                 <div>
                     <label htmlFor="age">Edad</label>
-                    <Field name="age" component="input" typpe="number"></Field>
+                    <Field
+                        name="age"
+                        component={MyField}
+                        validate={[isNumber, isRequired]}
+                        type="number"
+                    ></Field>
                 </div>
             </form>
         </div>
